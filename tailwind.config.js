9c3910aa0e31c935d2ca1200603d192ba5ffbde1
Dean Huang <hcd@mojopot.com>
2022-08-25 02:29:46 +0800

    use rem

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
+    // screens: {
+    //   sm: '640px',
+    //   // => @media (min-width: 640px) { ... }
+    //   lg: '1024px',
+    //   // => @media (min-width: 1024px) { ... }
+    //   xl: '1280px',
+    //   // => @media (min-width: 1280px) { ... }
+    // },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        md: '0',
+        lg: '0',
+      },
+    },
     extend: {},
   },
   plugins: [],
