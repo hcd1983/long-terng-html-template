cd63728b5c1ecf4b2b725b5771a14c7870013666
Dean Huang <hcd@mojopot.com>
2022-08-25 13:14:45 +0800

    use petite-vue

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
+import {createApp} from "petite-vue";
 import "./style/style.scss"
-console.log("hello world")
+createApp({
+   mobileMenuActive: false,
+    alertMe() {
+      alert('works')
+    },
+    mounted() {
+        document.body.classList.remove('opacity-0')
+    },
+    menuInit(el) {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > el.getBoundingClientRect().height) {
+                el.classList.add("bg-white")
+            } else {
+                el.classList.remove("bg-white")
+            }
+        })
+    }
+
+}).mount()
